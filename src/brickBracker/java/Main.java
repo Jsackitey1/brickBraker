4faeff817d72676ab1ec1c5232b0daa91dc9f1e8
Joseph Sackitey <sackiteyjoseph44@gmail.com>
2024-09-19 02:04:33 -0400

    update with obj.setVisible

--- src/brickBracker/java/Main.java
+++ src/brickBracker/java/Main.java
@@ -5,6 +5,7 @@ import javax.swing.JFrame;
 public class Main {
 
 	public static void main(String[] args) {
+			
 			JFrame obj= new JFrame();
 			Gameplay gameplay= new Gameplay();
 			obj.setBounds(10,10,700,600);
@@ -13,6 +14,10 @@ public class Main {
 			obj.setVisible(true);
 			obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			obj.add(gameplay);
+			obj.setVisible(true);
+			
+			
+			
 			
 	}
 
